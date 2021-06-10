e98db43dfeba44d28dabda04f7ce1dddec2693bd
Silvian73 <yurykuz65@gmail.com>
2021-06-10 17:30:54 +0300

    Created the main module and the game starter

---
+++ Assets/Code/MainModule/MainModuleFacade.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ThroughAThousandEyes.MainModule
+{
+    public class MainModuleFacade : IModuleFacade
+    {
+        private List<IModuleFacade> _moduleFacades;
+        private MainModuleUnityInterface _unityInterface;
+
+        public void InitializeModule(MainModuleFacade mainModuleFacade, bool isLoadingSavedGame, string saveData = "")
+        {
+            _unityInterface = new GameObject("Main Module Unity Interface").AddComponent<MainModuleUnityInterface>();
+            _unityInterface.EFixedUpdate += OnFixedUpdate;
+        }
+
+        public string SaveModule()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Tick(float deltaTime)
+        {
+            
+        }
+
+        public void InitializeGame(bool isLoadingSavedGame, string saveData = "")
+        {
+            // Create modules facades
+            _moduleFacades = new List<IModuleFacade>();
+            _moduleFacades.Add(this);
+            
+            // Initialize modules using facades
+            foreach (var facade in _moduleFacades)
+            {
+                facade.InitializeModule(this, isLoadingSavedGame, saveData);
+            }
+        }
+
+        public void SaveGame()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void OnFixedUpdate()
+        {
+            foreach (var facade in _moduleFacades)
+            {
+                facade.Tick(Time.fixedDeltaTime);
+            }
+        }
+    }
+}
